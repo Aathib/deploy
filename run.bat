5f5b6b836f79b07f2d699d9ee3b2475ef226e856
Aathib <38806320+Aathib@users.noreply.github.com>
2020-07-02 14:10:45 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@ set root=C:\Users\AppData\Local\Continuum\anaconda3
 
 call %root%/Scripts/activate.bat %root%
 
-cd C:\Users\813146\Documents\Full_data
+cd C:\Users\ab42764\Documents\Full_data
 
 python app1.py
